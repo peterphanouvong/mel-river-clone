5de45b0a78401e5a76b19b5c38ee347e5ca3a1b5
Peter Phanouvong <peterphanouvong@gmail.com>
2021-03-19 19:43:11 +1100

    merge-conflicts

--- src/App.js
+++ src/App.js
@@ -3,37 +3,8 @@ import { Route, Switch, useLocation } from "react-router-dom";
 
 // GSAP
 import gsap from "gsap";
-import { AnimatePresence } from "framer-motion";
-
 // Styles
 import "./styles/App.scss";
-import Header from "./components/header";
-import Navigation from "./components/navigation";
-
-import CaseStudies from "./pages/caseStudies";
-import Approach from "./pages/approach";
-import Services from "./pages/services";
-import About from "./pages/about";
-import Home from "./pages/home";
-
-const routes = [
-  { path: "/", name: "Home", Component: Home },
-  { path: "/case-studies", name: "caseStudies", Component: CaseStudies },
-  { path: "/approach", name: "approach", Component: Approach },
-  { path: "/services", name: "services", Component: Services },
-  { path: "/about-us", name: "about", Component: About }
-];
-
-function debounce(fn, ms) {
-  let timer;
-  return () => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      timer = null;
-      fn.apply(this, arguments);
-    }, ms);
-  };
-}
 
 // Components
 import Header from "./components/Header";
